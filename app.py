e5f7594ce2cc48a0737eed63da7681aa927371ad
Ian Foster <foster@anl.gov>
2023-04-22 17:02:03 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,25 @@
 from flask import Flask
+import json
+import requests
+
 app = Flask(__name__)
+
+globus_url = "https://transfer.api.globus.org/v0.10/private/web_stats"
+
+def get_data(url):
+    response = requests.get(url)
+
+    data = response.json()
+    json_data = json.dumps(data, indent=2)
+    data = json.loads(json_data)
+
+    bytes_value = data['new']['bytes']
+    bytes_value /= 10 ** 9
+    bytes_value %= 10 ** 7
+    print(int(bytes_value))
 
 @app.route('/')
 def hello_world():
-    return {'Hello, World!', 99}
+    # bytes = get_data(globus_url)
+    return 'Hello, World!'
+    #return {'Hello, World!', bytes}
