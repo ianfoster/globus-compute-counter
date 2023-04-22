9ca0bbcb60970f4b6a32e24a8a2568d46ed5faa1
Ian Foster <foster@anl.gov>
2023-04-22 17:52:59 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,24 @@ app = Flask(__name__)
 
 globus_url = "https://transfer.api.globus.org/v0.10/private/web_stats"
 
+initial_values = 
+
+"""
+Example:
+{
+  "new": {
+    "bytes": 1955402227052862012,
+    "files": 209318008246,
+    "time": "2023-04-22 22:47:02.125357"
+  },
+  "old": {
+    "bytes": 1955398391739215761,
+    "files": 209317930020,
+    "time": "2023-04-22 22:42:01.610209"
+  }
+}
+"""
+
 def get_data(url):
     response = requests.get(url)
 
@@ -21,10 +39,15 @@ def get_data(url):
     print(f'    extracted: {bytes_value}')
     return(bytes_value)
 
+last_value = get_data(globus_url)
+
 @app.route('/')
 def hello_world():
-    bytes = get_data(globus_url)
-    print(f' Bytes: {bytes}')
-    rv = f'{{"number": {bytes}}}'
+    next_value = get_data(globus_url)
+    if next_value == last_value:
+        next_value += 1
+    print(f' Bytes: {next_value}')
+    rv = f'{{"number": {next_value}}}'
     print(f'Response: {rv}')
+    last_value = next_value
     return rv
